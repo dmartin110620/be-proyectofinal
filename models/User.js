65e4b3857d9dfbd95ad51722e7d43726851e1864
dmartin110620 <109118261+dmartin110620@users.noreply.github.com>
2024-11-15 00:52:17 -0500

    auth tested

--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    universityID: { type: String, required: true },
-    contactNumber: { type: String, required: true },
+    universityID: { type: String, required: true, unique: true },
+    contactNumber: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     photo: { type: String } // opcional
 });
